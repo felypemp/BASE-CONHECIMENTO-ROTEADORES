6447060870fb4f2194a859ebf2917e70e524e2c4
Felipe <fmp.ti@outlook.com>
2023-02-12 12:49:34 -0300

    Tabelas foram adicionadas a página de comando. Página onde elas ficavam anteriormente foi removida

--- js/comandosEquipamento.js
+++ js/comandosEquipamento.js
@@ -1,1 +1,16 @@
 const btn_edd = document.querySelector(".btnEdd");
+const btn_cisco = document.querySelector(".btnCisco");
+const tabela_edd = document.querySelector("#edd");
+const tabela_cisco = document.querySelector("#cisco");
+
+
+//Criando função para esconder e mostrar os scripts
+btn_edd.addEventListener("click", () =>{
+    tabela_edd.classList.remove("esconder")
+    tabela_cisco.classList.add("esconder")
+})
+
+btn_cisco.addEventListener("click", () =>{
+    tabela_cisco.classList.remove("esconder");
+    tabela_edd.classList.add("esconder")
+})
